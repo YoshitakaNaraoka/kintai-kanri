e5be6a8c119d1e822dffd70c102e1388ab741f0f
Placeless <mikoprpr@hotmail.com>
2024-04-19 04:39:56 +0900

    とりあえず削除

--- src/app.rs
+++ src/app.rs
@@ -16,38 +16,6 @@ struct LoginArgs<'a> {
     pass: &'a str,
 }
 
-pub struct Model {
-    link: ComponentLink<Self>,
-}
-
-pub enum Msg {
-    GoToHome,
-    GoToDetail,
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::GoToHome => {
-                // Tauriコマンドを呼び出してホーム画面へ遷移
-                tauri::command::spawn("go_to_home".to_string());
-            }
-            Msg::GoToDetail => {
-                // Tauriコマンドを呼び出して詳細画面へ遷移
-                tauri::command::spawn("go_to_detail".to_string());
-            }
-        }
-        false
-    }
-}
-
 #[function_component(App)]
 pub fn app() -> Html {
     let login_input_ref = use_node_ref();
@@ -127,12 +95,3 @@ pub fn app() -> Html {
         </main>
     }
 }
-
-fn view(&self) -> Html {
-    html! {
-        <div>
-            <button onclick=self.link.callback(|_| Msg::GoToHome)>{"Go to Home"}</button>
-            <button onclick=self.link.callback(|_| Msg::GoToDetail)>{"Go to Detail"}</button>
-        </div>
-    }
-};
