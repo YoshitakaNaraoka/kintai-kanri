37fa5477380a4a72eb8bf892338bd810497e1039
Placeless <mikoprpr@hotmail.com>
2024-04-20 13:40:34 +0900

    add hello.rs

--- src/hello.rs
+++ src/hello.rs
@@ -2,41 +2,43 @@
 
 use yew::prelude::*;
 
-pub struct App {}
+pub mod hello {
+  pub struct App {}
 
-pub enum Msg {
-    // ログインボタンがクリックされた時のメッセージ
-    LoginButtonClicked,
-}
+  pub enum Msg {
+      // ログインボタンがクリックされた時のメッセージ
+      LoginButtonClicked,
+  }
 
-impl Component for App {
-    type Message = Msg;
-    type Properties = ();
+  impl Component for App {
+      type Message = Msg;
+      type Properties = ();
 
-    // コンポーネントの生成
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self {}
-    }
+      // コンポーネントの生成
+      fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+          Self {}
+      }
 
-    // メッセージのハンドリング
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::LoginButtonClicked => {
-                // ログインボタンがクリックされたときの処理
-                // Tauriのコマンドを呼び出してUIを表示する
-                tauri::command::spawn("show_ui".to_string());
-            }
-        }
-        false
-    }
+      // メッセージのハンドリング
+      fn update(&mut self, msg: Self::Message) -> ShouldRender {
+          match msg {
+              Msg::LoginButtonClicked => {
+                  // ログインボタンがクリックされたときの処理
+                  // Tauriのコマンドを呼び出してUIを表示する
+                  tauri::command::spawn("show_ui".to_string());
+              }
+          }
+          false
+      }
 
-    // UIのレンダリング
-    fn view(&self) -> Html {
-        html! {
-            <div>
-                <h1>{"Hello, world!"}</h1>
-                <button onclick=|_| Msg::LoginButtonClicked>{"Login"}</button>
-            </div>
-        }
-    }
+      // UIのレンダリング
+      fn view(&self) -> Html {
+          html! {
+              <div>
+                  <h1>{"Hello, world!"}</h1>
+                  <button onclick=|_| Msg::LoginButtonClicked>{"Login"}</button>
+              </div>
+          }
+      }
+  }
 }
