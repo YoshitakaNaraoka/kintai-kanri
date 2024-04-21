0013afabc6ed8dac5fb35cf97b2b9722ada8bbb2
Placeless <mikoprpr@hotmail.com>
2024-04-21 22:52:28 +0900

    近づいてはいる気がする

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
-use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
+use wasm_bindgen::prelude::*;
+
 
 #[derive(Serialize, Deserialize)]
 struct LoginResponse {
@@ -15,27 +16,10 @@ struct LoginArgs {
 }
 
 // MessageComponent用のプロパティ型
+#[derive(Properties, Clone, PartialEq)]
 struct MessageComponentProps {
     message: String,
 }
-
-// MessageComponent用のプロパティ型
-#[derive(Properties, PartialEq)] // PropertiesとPartialEqを自動導出する
-struct MessageComponentProps {
-    message: String,
-}
-
-
-impl Properties for MessageComponentProps {
-    // プロパティ型として適切なコンポーネントを指定
-    type Builder = ();
-    
-    fn builder() -> Self::Builder {
-        
-    }
-}
-
-
 
 // メッセージを表示するコンポーネント
 #[function_component(MessageComponent)]
@@ -45,10 +29,11 @@ fn message_component(props: &MessageComponentProps) -> Html {
     }
 }
 
+
 #[function_component(App)]
 pub fn app() -> Html {
     let login_input_ref = NodeRef::default();
-    let login_msg = use_state(|| String::new());
+    let login_msg = use_state(String::new);
 
     let on_login_submit = {
         let login_input_ref = login_input_ref.clone();
@@ -66,13 +51,20 @@ pub fn app() -> Html {
             let login_args = LoginArgs { mail: mail_value, pass: pass_value };
 
             let login_future = async move {
-                match invoke("login", JsValue::from_serde(&login_args).unwrap()).await {
+                match serde_wasm_bindgen::to_value(&login_args) {
                     Ok(js_value) => {
-                        let login_response: LoginResponse = js_value.into_serde().unwrap();
-                        login_msg.set(login_response.message);
+                        match invoke("login", &js_value).await {
+                            Ok(js_value) => {
+                                let login_response: LoginResponse = js_value.into_serde().unwrap();
+                                login_msg.set(login_response.message);
+                            },
+                            Err(_) => {
+                                login_msg.set("Failed to communicate with server".to_string());
+                            }
+                        }
                     },
                     Err(_) => {
-                        login_msg.set("Failed to communicate with server".to_string());
+                        login_msg.set("Failed to serialize login arguments".to_string());
                     }
                 }
             };
@@ -103,7 +95,7 @@ pub fn app() -> Html {
             </form>
 
             // MessageComponentを呼び出す際にPropsを渡す
-            <MessageComponent message=&login_msg />
+            <MessageComponent message=login_msg.clone() />
         </main>
     }
 }
